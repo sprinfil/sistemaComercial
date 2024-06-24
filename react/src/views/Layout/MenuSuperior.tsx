c500b5815e4335f0114a70c1f204c466fa7168de
Jeremy Ojeda Ceseña <112501128+sprinfil@users.noreply.github.com>
2024-06-23 17:38:37 -0700

    menu

--- react/src/views/Layout/MenuSuperior.tsx
+++ react/src/views/Layout/MenuSuperior.tsx
@@ -24,6 +24,53 @@ const MenuSuperior = () => {
 
     const { setToken, setUser, user } = useStateContext();
 
+    const opciones = [
+        {
+            titulo: "Usuarios",
+            opciones: [
+                {
+                    titulo: "Consulta",
+                    descripcion: "A un usuario o toma"
+                },
+                {
+                    titulo: "Contratos",
+                    descripcion: "Genera un nuevo contrato"
+                }
+            ]
+        },
+        {
+            titulo: "Poligonos Geograficos",
+            opciones: [
+                {
+                    titulo: "Ver poligonos",
+                    descripcion: "Administra las posiciones geograficas de las rutas y libros."
+                },
+            ]
+        },
+        {
+            titulo: "Ordenes de Trabajo",
+            opciones: [
+                {
+                    titulo: "Generar Ordenes de Trabajo",
+                    descripcion: "Genera ordenes para operadores de campo"
+                },
+                {
+                    titulo: "Asignar Ordenes de Trabajo",
+                    descripcion: "Asigna Ordenes de trabajo para operadores de campo"
+                },
+            ]
+        },
+        {
+            titulo: "Monitores",
+            opciones: [
+                {
+                    titulo: "Monitores",
+                    descripcion: "Haz consultas de cualquier entidad que necesites."
+                },
+            ]
+        }
+    ]
+
     const logout = (e: MouseEvent<SVGSVGElement>): void => {
         e.preventDefault();
         axiosClient.post("/logout")
@@ -46,40 +93,31 @@ const MenuSuperior = () => {
             <div className='h-full p-[10px] left-[100px] absolute flex items-center'>
                 <NavigationMenu>
                     <NavigationMenuList>
-
-                        <NavigationMenuItem>
-                            <NavigationMenuTrigger>Usuarios</NavigationMenuTrigger>
-                            <NavigationMenuContent>
-                                <ul className='px-[10px] py-[10px] w-[400px]'>
-                                    <li className='hover:hover:bg-accent p-3 rounded-md hover:cursor-pointer ease-in duration-100'>
-                                        <div className="mb-1 text-md font-medium">
-                                            Consulta
-                                        </div>
-                                        <p className="text-sm leading-tight text-muted-foreground">
-                                            Usuario o Toma.
-                                        </p>
-                                    </li>
-                                    <li className='hover:hover:bg-accent p-3 rounded-md hover:cursor-pointer ease-in duration-100'>
-                                        <div className="mb-1 text-md font-medium">
-                                            Contratos
-                                        </div>
-                                        <p className="text-sm leading-tight text-muted-foreground">
-                                            Genera un nuevo contrato.
-                                        </p>
-                                    </li>
-
-                                </ul>
-                            </NavigationMenuContent>
-                        </NavigationMenuItem>
-
-
-
+                        {opciones.map((opcion, key) => (
+                            <NavigationMenuItem>
+                                <NavigationMenuTrigger>{opcion.titulo}</NavigationMenuTrigger>
+                                <NavigationMenuContent>
+                                    <ul className='px-[10px] py-[10px] w-[600px]'>
+                                        {opcion.opciones.map((opcion, index) => (
+                                            <li className='hover:hover:bg-accent p-3 rounded-md hover:cursor-pointer ease-in duration-100'>
+                                                <div className="mb-1 text-md font-medium">
+                                                    {opcion.titulo}
+                                                </div>
+                                                <p className="text-sm leading-tight text-muted-foreground">
+                                                    {opcion.descripcion}
+                                                </p>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </NavigationMenuContent>
+                            </NavigationMenuItem>
+                        ))}
                     </NavigationMenuList>
                 </NavigationMenu>
             </div>
             <div className=' h-full w-[200px] absolute right-5 flex items-center  justify-center gap-3'>
-                <div className=' px-2 py-1 rounded-md hover:bg-accent cursor-pointer ease-in duration-100'  onClick={logout} >
-                    <FontAwesomeIcon icon={faRightFromBracket} className='rotate-180'/>
+                <div className=' px-2 py-1 rounded-md hover:bg-accent cursor-pointer ease-in duration-100' onClick={logout} >
+                    <FontAwesomeIcon icon={faRightFromBracket} className='rotate-180' />
                 </div>
                 <p>{user.name}</p>
                 <Avatar>
